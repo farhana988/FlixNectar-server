56121525b7fd93dd1608a3e838da154b8fc689ad
farhana988 <farhana744255@gmail.com>
2024-12-03 18:50:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000
+
+
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('api is running')
+})
+
+app.listen(port,()=>{
+    console.log(`api running on port ${port}`)
+})
